bf66ff124361d85bdf633815aa2dc45822980b13
Vladislav <wleeeysss@gmail.com>
2024-09-13 17:09:46 +0300

    change cases

--- src/cli.rs
+++ src/cli.rs
@@ -1,8 +1,25 @@
 use compare_packages::architecture_support::Arch;
+use compare_packages::branches::Branch;
 use inquire::Select;
 
 pub fn select_architecture(architectures: Vec<Arch>) -> Arch {
     Select::new("Architecture that interests you:", architectures)
         .prompt()
         .expect("It is necessary to choose an architecture")
+}
+
+pub fn select_branches() -> (Branch, Branch) {
+    let mut branches: Vec<Branch> = vec![Branch::Sisyphus, Branch::P9, Branch::P10, Branch::P11];
+
+    let branch_one = Select::new("Branch you want to compare:", branches.clone())
+        .prompt()
+        .expect("It is necessary to choose an branch");
+
+    branches.retain(|branch| branch.as_str() != branch_one.as_str());
+
+    let branch_two = Select::new("Branch you want to compare it with:", branches)
+        .prompt()
+        .expect("It is necessary to choose an branch");
+
+    (branch_one, branch_two)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
+use crate::cli::select_branches;
 use compare_packages::api_alt_json_templates::Package;
-use compare_packages::branches::Branch;
 use compare_packages::{architecture_support, cases, FetchError};
 use env_logger::Builder;
 use log::LevelFilter;
@@ -14,26 +14,29 @@ async fn main() {
         .filter(None, LevelFilter::Info)
         .init();
 
+    let (branch_one, branch_two) = select_branches();
+
     log::info!("Fetching supported architectures...");
-    let architectures = architecture_support::fetch_supported_architectures().await;
+    let architectures =
+        architecture_support::fetch_supported_architectures(branch_one, branch_two).await;
 
     let selected_arch = cli::select_architecture(architectures);
 
-    log::info!("Fetching those packages that are only in sisyphus but not in p10...");
+    log::info!("Fetching those packages that are only in {branch_one} but not in {branch_two}...");
     let only_sisyphus_packages =
-        cases::fetch_only_packages_from_selected_branch(Branch::Sisyphus, &selected_arch)
+        cases::fetch_only_packages_from_a_branch(branch_one, branch_two, &selected_arch)
             .await
             .unwrap_or_else(fetch_error_for_output);
 
-    log::info!("Fetching those packages that are only in p10 but not in sisyphus...");
+    log::info!("Fetching those packages that are only in {branch_two} but not in {branch_one}...");
     let only_p10_packages =
-        cases::fetch_only_packages_from_selected_branch(Branch::P10, &selected_arch)
+        cases::fetch_only_packages_from_a_branch(branch_one, branch_two, &selected_arch)
             .await
             .unwrap_or_else(fetch_error_for_output);
 
-    log::info!("Fetching those packages that have more version-release in sisyphus than in p10...");
+    log::info!("Fetching those packages that have more version-release in {branch_one} than in {branch_two}...");
     let packages_vr_more_in_sisyphus_than_p10 =
-        cases::fetch_vr_more_in_sisyphus_than_p10(&selected_arch)
+        cases::fetch_vr_more_in_a_than_b_branch(branch_one, branch_two, &selected_arch)
             .await
             .unwrap_or_else(fetch_error_for_output);
 
